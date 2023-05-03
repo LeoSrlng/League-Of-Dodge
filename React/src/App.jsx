d9e8376d4088005128356e8dcac0ab5515020400
LeoSrlng <leo.sarlange.pro@outlook.fr>
2023-05-03 17:50:26 +0200

    supression des fichiers superflus

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import axios from "axios";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = React.useState(0);
 	const [liste, setListe] = React.useState("");
 
 	React.useEffect(() => {
@@ -22,22 +19,7 @@ function App() {
 
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
 			<h1>{liste}</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	);
 }
